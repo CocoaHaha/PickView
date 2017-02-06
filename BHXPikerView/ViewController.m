96bb639401a9e8af8e47c34112d6b7e7a3bbae44
demo <baohongxubhx@sina.com>
2017-02-07 00:01:44 +0800

    Initial Commit

---
+++ BHXPikerView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BHXPikerView
+//
+//  Created by 包红旭 on 2017/2/7.
+//  Copyright © 2017年 包红旭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
