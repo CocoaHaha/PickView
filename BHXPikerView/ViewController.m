83c83e756997e6bfdca9cf75629b59ea82f5db45
CocoaHaha <1076800596@qq.com>
2017-07-27 14:16:56 +0800

    first commit

--- BHXPikerView/ViewController.m
+++ BHXPikerView/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "BHXPikerView.h"
+#import "Masonry.h"
+@interface ViewController ()<BHXDatePikerViewDelegate>
+@property (nonatomic,strong) UIButton *chooseTimeBtn;
+@property (nonatomic,strong)BHXPikerView *timeV;
 
 @end
 
@@ -16,7 +19,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.chooseTimeBtn = [[UIButton alloc]init];
+    [self.chooseTimeBtn setTitle:@"选 择 时 间" forState:UIControlStateNormal];
+    [self.chooseTimeBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:self.chooseTimeBtn];
+    [self.chooseTimeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view.mas_centerX);
+        make.top.equalTo(@200);
+    }];
+    
+    [self.chooseTimeBtn addTarget:self action:@selector(chooseTime:) forControlEvents:UIControlEventTouchUpInside];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (BHXPikerView *)timeV{
+    if (!_timeV) {
+        self.timeV = [[BHXPikerView alloc]initWithFrame:self.view.bounds type:UIDatePickerModeDateAndTime];
+        self.timeV.delegate = self;
+    }
+    return _timeV;
+}
+
+- (void)chooseTime:(UIButton *)sender {
+    [self.view addSubview:self.timeV];
+    //初始化时间
+    if (sender.titleLabel.text.length != 0 && ![sender.titleLabel.text isEqualToString:@"选 择 时 间"]) {
+        [self.timeV setNowTime:sender.titleLabel.text];
+    }
+}
+
+#pragma mark - TXTimeDelegate
+//当时间改变时触发
+- (void)changeTime:(NSDate *)date{
+    [self.chooseTimeBtn setTitle:[self.timeV stringFromDate:date] forState:UIControlStateNormal];
+
+    NSLog(@"%@",date);
+}
+
+//确定时间
+- (void)determine:(NSDate *)date{
+    [self.chooseTimeBtn setTitle:[self.timeV stringFromDate:date] forState:UIControlStateNormal];
+    
 }
 
 
